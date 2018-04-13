1b20c0ab15fb1c7b59f79b894200d47506a1eddc
xiaomuzhu <meituandxy@gmail.com>
2018-04-13 13:29:31 +0800

    feat: add reset css

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import FastClick from 'fastclick';
 import VueLazyload from 'vue-lazyload';
 import VueIconFont from 'vue-icon-font';
 
+import 'normalize.css';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
