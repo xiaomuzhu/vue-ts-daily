c8deeccc05775b30499a48e56562f27a41347a46
xiaomuzhu <meituandxy@gmail.com>
2018-04-13 13:28:27 +0800

    feat: add iconfont

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import Vue from 'vue';
 
 import FastClick from 'fastclick';
 import VueLazyload from 'vue-lazyload';
+import VueIconFont from 'vue-icon-font';
 
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import '@/assets/iconfont.js';
 
 // 兼容毒瘤ios的300ms延迟问题
 if ('addEventListener' in document) {
@@ -15,6 +17,7 @@ if ('addEventListener' in document) {
   }, false);
 }
 
+Vue.use(VueIconFont);
 Vue.use(VueLazyload, { preLoad: 1 });
 
 Vue.config.productionTip = false;
