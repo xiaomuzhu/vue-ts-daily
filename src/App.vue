8730e5178e01d03eecdecdab36e9f0288bfddcc8
xiaomuzhu <meituandxy@gmail.com>
2018-04-13 14:17:27 +0800

    feat: add vant

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <main id="app">
     <Header></Header>
-    <!-- <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-      </div> -->
     <content>
       <router-view />
     </content>
