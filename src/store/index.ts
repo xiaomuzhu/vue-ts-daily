6287aeef807871b196a2f3d484b72334ac1532a6
xiaomuzhu <meituandxy@gmail.com>
2018-04-21 22:27:04 +0800

    feat: vuex 持久化

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import * as Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 
 import state, { State } from './state';
 import mutations from './mutations';
@@ -8,10 +9,14 @@ import getters from './getters';
 
 Vue.use(Vuex);
 
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+})
 
 export default new Vuex.Store<State>({
   state,
   mutations,
   actions,
   getters,
+  plugins: [vuexLocal.plugin],
 });
