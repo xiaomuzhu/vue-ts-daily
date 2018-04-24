63132eefdde63218412c1ba0c531d2ca619ed23f
xiaomuzhu <meituandxy@gmail.com>
2018-04-25 04:54:46 +0800

    feat: add mainfest

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,14 +1,28 @@
 /* tslint:disable:no-console */
+// declare var self: ServiceWorkerGlobalScope;
+
+const cacheName = 'daily-v1';
 
 import { register } from 'register-service-worker';
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB',
-      );
+      self.addEventListener('install', (event: any) => {
+        event.waitUntil(
+          caches.open(cacheName)
+          .then((cache) => cache.addAll([
+            './js/main.js',
+            './js/article.js',
+            './images/newspaper.svg',
+            './css/site.css',
+            './data/latest.json',
+            './data/data-1.json',
+            './article.html',
+            './index.html',
+          ])),
+        );
+      });
     },
     cached() {
       console.log('Content has been cached for offline use.');
